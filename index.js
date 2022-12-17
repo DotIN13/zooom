8e5ab88d33aaff4feb50c8d2ad3e7e2f8ae6d427
DotIN13 <dotin13@126.com>
2022-12-17 15:57:27 +0800

    Add index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+import PortalController from "./javascript/controllers/portal_controller.js";
+import ZoomerangController from "./javascript/controllers/zoomerang_controller.js";
+
+export { PortalController, ZoomerangController };
